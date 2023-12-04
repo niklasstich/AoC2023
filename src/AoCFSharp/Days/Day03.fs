97faa99d7da0563c0a92a1bd39d7424afecf6d85
Niklas Stich <32730297+niklasstich@users.noreply.github.com>
2023-12-04 19:52:45 +0100

    Day 03 F#

--- src/AoCFSharp/Days/Day03.fs
+++ src/AoCFSharp/Days/Day03.fs
@@ -45,25 +45,20 @@ let findNumbersNextToSymbols symbols numbers =
 
 let findGearRatios (symbols: SymbolPosition array) (numbers: NumberPosition array) =
     let numsNextToGears (numbers: NumberPosition array) (symbol: SymbolPosition) =
-        crossProductArray [|symbol|] numbers |> Array.map (fun ((ax,ay,_),(bx,by,n))->((ax,ay),(bx,by,n))) |> Array.filter areNeighbors |> Array.map snd
-    let addTuples (a: NumberPosition array) =
-        let _,_,n1 = a[0]
-        let _,_,n2 = a[1]
-        n1 * n2
-        
-    symbols |> Array.map (numsNextToGears numbers) |> Array.filter (fun a -> Array.length a = 2) |> Array.map addTuples
+        crossProductArray [| symbol |] numbers
+        |> Array.map (fun ((ax, ay, _), (bx, by, n)) -> ((ax, ay), (bx, by, n)))
+        |> Array.filter areNeighbors
+        |> Array.map snd
 
-let example =
-    @"467..114..
-...*......
-..35..633.
-......#...
-617*......
-.....+.58.
-..592.....
-......755.
-...$.*....
-.664.598.."
+    let addTuples (a: NumberPosition array) =
+        let _, _, n1 = a[0]
+        let _, _, n2 = a[1]
+        n1 * n2
+
+    symbols
+    |> Array.map (numsNextToGears numbers)
+    |> Array.filter (fun a -> Array.length a = 2)
+    |> Array.map addTuples
 
 let part1 (input: string array) =
     let numbersNextToSymbols =
@@ -86,13 +81,13 @@ let part2 (input: string array) =
         (input |> Array.mapi (fun i line -> (i, line)) |> Array.collect findNumbers)
 
     (input
-    |> Array.mapi (fun i line -> (i, line))
-    |> Array.collect findSymbols
-    |> Array.where (fun (_, _, c) -> c = '*')
-    |> findGearRatios
-    <| numbers) |> Array.sum |> string
+     |> Array.mapi (fun i line -> (i, line))
+     |> Array.collect findSymbols
+     |> Array.where (fun (_, _, c) -> c = '*')
+     |> findGearRatios
+     <| numbers)
+    |> Array.sum
+    |> string
 
 let solution1 = 559667
-
-
-let debug = example |> splitLines |> part2
+let solution2 = 86841457
