dc588abe74f7c5aae18dcf2c597cdca4ee09927f
Niklas Stich <32730297+niklasstich@users.noreply.github.com>
2023-12-21 19:31:08 +0100

    Day 06

--- src/AoCFSharp/Days/Day06.fs
+++ src/AoCFSharp/Days/Day06.fs
@@ -1,12 +1,30 @@
 module AoCFSharp.Days.Day06
 
 open System
+open AoCFSharp.Lib
+open Shared
 
-let input' =
-    [|(35,212);(93, 2060);(73,1201);(66,1044)|]
+let input' = [| (35L, 212L); (93L, 2060L); (73L, 1201L); (66L, 1044L) |]
+let input'' = (35937366L, 212206012011044L)
+
+let solveCountAsQuadraticEquation (t: int64, d: int64) =
+    //solve x*(t-x)-d=0 and return distance of those points (rounded up because discrete time values)
+    let x1, x2 = quadraticEq -1.0 (float t) (float (-d))
+    let x1' = Math.Ceiling(x1)
+    let x2' = Math.Ceiling(x2)
+    int64(max x1' x2' - min x1' x2')
+ 
+// naive linear solution   
+// let mapTimeToDistances (i, d) =
+//     (seq { for j in 0..i -> (j * (i - j)) }, d)
+//     
+// let countWinners (d, target) =
+//     d |> Seq.where (fun d' -> d' > target) |> Seq.length
 
 let part1 (input: string array) =
-    //input' |> Array.map (fun s -> fst s |> ())
-    ()
+    input' |> Array.map solveCountAsQuadraticEquation |> Array.fold (*) 1L |> string
+    
 let part2 (input: string array) =
-    ()
+    input'' |> solveCountAsQuadraticEquation |> string
+
+let solution1 = 114400
--- src/AoCFSharp/Lib/Shared.fs
+++ src/AoCFSharp/Lib/Shared.fs
@@ -9,3 +9,13 @@ let intersection (lower: 'a, upper: 'a) (bl: 'a, bu: 'a): 'a * 'a =
     let left: 'a = max lower bl
     let right: 'a = (min upper bu)
     (left, right)
+
+(*
+    Standard quadratic equation (Mitternachtsformel)
+*)
+let quadraticEq a b c =
+    let root = sqrt (b * b - 4.0 * a * c)
+    let denom = 2.0 * a
+    let x1 = (-b + root) / denom
+    let x2 = (-b - root) / denom
+    (x1, x2)
